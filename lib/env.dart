3155f554e602a99777131a7cd099f7b99cfefd43
Alansherhan <alansherhan10@gmail.com>
2026-01-17 12:33:11 +0530

    feat: Add environment constants for API endpoints and JWT token storage key.

--- lib/env.dart
+++ lib/env.dart
@@ -1,4 +1,4 @@
 const kBaseUrl = '$kIpAddress:3000/api';
 const kImageUrl = '$kIpAddress:3000';
-const kIpAddress = 'http://192.168.1.17';
+const kIpAddress = 'http://192.168.1.9';
 const kTokenStorageKey = "jwt_token";
