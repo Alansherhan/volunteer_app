6e6e70a5b52b35769ccdccf28ef65a36f0dc3f5d
Alansherhan <alansherhan10@gmail.com>
2026-01-24 17:27:28 +0530

    feat: Integrate Firebase and set up Firebase Cloud Messaging.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,41 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:volunteer_app/firebase_options.dart';
 import 'package:volunteer_app/screens/splash_screen.dart';
+import 'package:volunteer_app/services/fcm_service.dart';
 import 'package:volunteer_app/theme/app_theme.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized(); // ADD THIS LINE
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Firebase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // Set up background message handler
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    // Initialize FCM after app starts
+    _initFcm();
+  }
+
+  Future<void> _initFcm() async {
+    await FcmService().initialize();
+  }
 
   @override
   Widget build(BuildContext context) {
