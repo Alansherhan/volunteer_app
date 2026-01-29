f44eec766896661804459e937bcb7f9717e8117b
Alansherhan <alansherhan10@gmail.com>
2026-01-29 19:16:09 +0530

    feat: Define API base URL, image URL, IP address, and token storage key constants.

--- lib/env.dart
+++ lib/env.dart
@@ -1,4 +1,4 @@
 const kBaseUrl = '$kIpAddress:3000/api';
 const kImageUrl = '$kIpAddress:3000';
-const kIpAddress = 'http://192.168.1.12';
+const kIpAddress = 'http://192.168.1.14';
 const kTokenStorageKey = "jwt_token";
