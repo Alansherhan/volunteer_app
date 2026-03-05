aa030efc0f52b896e45c56e1fae64f945c0d7c6f
Alansherhan <alansherhan10@gmail.com>
2026-03-05 17:16:34 +0530

    fix: Update IP address in env.dart for correct API endpoint

--- lib/env.dart
+++ lib/env.dart
@@ -1,4 +1,4 @@
 const kBaseUrl = '$kIpAddress:3000/api';
 const kImageUrl = '$kIpAddress:3000';
-const kIpAddress = 'http://192.168.1.16';
+const kIpAddress = 'http://192.168.43.7';
 const kTokenStorageKey = "jwt_token";
