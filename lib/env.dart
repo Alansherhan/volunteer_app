9da6bb9724fd7354d0e89a35fd559ffaeffa9f5b
Alansherhan <alansherhan10@gmail.com>
2026-03-10 01:59:41 +0530

    chore: Update production backend IP address to new Render URL.

--- lib/env.dart
+++ lib/env.dart
@@ -3,5 +3,5 @@ const kImageUrl = '$kIpAddress';
 // For local development, use your local IP:
 // const kIpAddress = 'http://192.168.29.170:3000';
 // For production (Render):
-const kIpAddress = 'https://relieflow-backend.onrender.com';
+const kIpAddress = 'https://relieflow-backend-stu1.onrender.com';
 const kTokenStorageKey = "jwt_token";
